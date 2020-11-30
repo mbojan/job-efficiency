a60352d80b5c419800303464f785aa3e3e7933c9
Michal Bojanowski <michal2992@gmail.com>
2020-11-30 22:50:41 +0100

    Plots

--- jobs.R
+++ jobs.R
@@ -29,6 +29,41 @@ c172 <- readr::read_csv(
     ft = lubridate::days(ft_days) + ft_time,
     et_days = as.integer(et_days),
     et_time = lubridate::hms(et_time),
-    et = lubridate::days(et_days) + et_time
+    et = lubridate::days(et_days) + et_time,
+    ft_nm = as.numeric(ft) / Distance
   ) %>%
-  filter(Distance > 0, ft_time > lubridate::hms("00:1:0.0"))
+  filter(
+    Distance > 0, 
+    ft_time > lubridate::hms("00:1:0.0"), 
+    ft_days == 0, 
+    Income > 0,
+    Income < 10^5
+    )
+
+
+# Pay per nm --------------------------------------------------------------
+
+ggplot(c172, aes(y=Income)) + geom_histogram()
+
+c172 %>%
+  ggplot(aes(x=Distance, y=Income)) +
+  geom_point() +
+  scale_y_log10()
+
+model <- lm( I(log(Income)) ~ Distance, data=c172)
+summary(model)
+
+
+# Flight time per distance ------------------------------------------------
+
+
+c172 %>%
+  ggplot(aes(x=ft_nm)) +
+  geom_histogram() +
+  scale_x_log10()
+
+c172 %>%
+  ggplot(aes(y = ft_nm, x = Distance)) +
+  geom_point() +
+  scale_x_log10()
+  
